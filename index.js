7300b44b06a36d2ed2725ae9b3fddcecf2645c53
Abhijeet Kumar <104857874+abhijeete10@users.noreply.github.com>
2024-05-08 13:02:34 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,8 +6,32 @@ const port = 3000;
 const API_URL = "https://20.255.56.144/test";
 app.get("/numbers/e", async (req, res) => {
     try {
-      const result = await axios.get(API_URL + "/e");
-      res.send(Numbers: result);
+      const result = await axios.get(API_URL + "/even");
+      res.send(JSON.stringify(result.numbers));
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
+  });
+  app.get("/numbers/f", async (req, res) => {
+    try {
+      const result = await axios.get(API_URL + "/fibo");
+      res.send(res.data);
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
+  });
+  app.get("/numbers/r", async (req, res) => {
+    try {
+      const result = await axios.get(API_URL + "/rand");
+      res.send({Numbers: "result"});
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
+  });
+  app.get("/numbers/p", async (req, res) => {
+    try {
+      const result = await axios.get(API_URL + "/primes");
+      res.send({Numbers: "result"});
     } catch (error) {
       res.status(404).send(error.message);
     }
@@ -15,3 +39,4 @@ app.get("/numbers/e", async (req, res) => {
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
+  
